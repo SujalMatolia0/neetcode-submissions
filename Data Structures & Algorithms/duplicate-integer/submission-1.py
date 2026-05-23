907ccdb0aaf4a12f315dca065889249bd2ade3f0
Sujal aka TkBoom <128312923+SujalMatolia0@users.noreply.github.com>
2026-05-24 00:29:23 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        num = []
+        for i in nums:
+            if(i in num):
+                return True
+            num.append(i)
+        return False
